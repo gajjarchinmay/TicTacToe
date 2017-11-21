ba0690810f9db51662ab01715f3df73f500e4597
Chinmay Gajjar <chinmay.gajjar@einfochips.com>
2017-11-21 11:12:38 +0530

    Initial Commit

---
+++ app/src/main/java/com/tictactoe/tictactoe/MainActivityKT.kt
@@ -0,0 +1,12 @@
+package com.tictactoe.tictactoe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityKT : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_kt)
+    }
+}
